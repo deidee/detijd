6983f6a5157b1be308f0d9bf86a630b1eab94685
acj <gitlab@acjs.net>
2019-11-26 12:00:32 +0100

    Draw something.

--- class.detijd.php
+++ class.detijd.php
@@ -8,6 +8,9 @@ class Detijd
     private $height = 300;
     private $width = 300;
     private $type = 'png';
+    private $x = 0;
+    private $y = 0;
+    private $size = 24;
 
     public function __construct($settings = [])
     {
@@ -17,6 +20,17 @@ class Detijd
         $this->im = new Imagick();
         $this->im->newImage($this->width, $this->height, new ImagickPixel(self::WHITE));
         $this->im->setImageFormat($this->type);
+
+        $draw = new ImagickDraw();
+        $draw->setViewbox(0, 0, $this->width, $this->height);
+        $draw->setStrokeWidth(0);
+
+        $draw->setFillColor(new ImagickPixel('#000000'));
+        $x2 = $this->x + $this->size + mt_rand(-1, 2);
+        $y2 = $this->y + $this->size + mt_rand(-1, 2);
+        $draw->rectangle($this->x, $this->y, $x2, $y2);
+
+        $this->im->drawImage($draw);
     }
 
     public function __toString()
