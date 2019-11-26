0ae93b5d95e6f887bbe17974f1d30a14bbcfe1cf
acj <gitlab@acjs.net>
2019-11-26 11:37:05 +0100

    Display image.

--- class.detijd.php
+++ class.detijd.php
@@ -2,16 +2,32 @@
 
 class Detijd
 {
+    const WHITE = '#ffffff';
+
     private $format = 'H:i:s';
+    private $height = 300;
+    private $width = 300;
+    private $type = 'png';
 
     public function __construct($settings = [])
     {
         // Set default timezone. See https://www.php.net/manual/en/timezones.europe.php.
         date_default_timezone_set('Europe/Amsterdam');
+
+        $this->im = new Imagick();
+        $this->im->newImage($this->width, $this->height, new ImagickPixel(self::WHITE));
+        $this->im->setImageFormat($this->type);
     }
 
     public function __toString()
     {
         return date($this->format);
+    }
+
+    public function display()
+    {
+        header('Content-Type: ' . $this->im->getImageMimeType());
+
+        echo $this->im->getImageBlob();
     }
 }
---
+++ test/detijd.png.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once '../class.detijd.php';
+
+$tijd = new Detijd;
+$tijd->display();
